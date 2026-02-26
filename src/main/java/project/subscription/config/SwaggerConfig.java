4b581c527ed4a54571dfff127f476dc68793a920
sangjae <sjsj00718@gmail.com>
2026-02-26 16:36:36 +0900

    feat: 모니터링 의존성 추가

--- src/main/java/project/subscription/config/SwaggerConfig.java
+++ src/main/java/project/subscription/config/SwaggerConfig.java
@@ -3,7 +3,11 @@ package project.subscription.config;
 
 import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
 import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 
 @SecurityScheme(
         name = "bearerAuth",
@@ -13,4 +17,12 @@ import org.springframework.context.annotation.Configuration;
 )
 @Configuration
 public class SwaggerConfig {
+
+    @Bean
+    @Profile(value = "prod")
+    public OpenAPI openAPI() {
+        return new OpenAPI()
+                .addServersItem(new Server()
+                        .url("https://xn--zb0b0h61ozwg9lg1uep6c.site"));
+    }
 }
